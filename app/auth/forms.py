12b79f2d7167c69477787bf03774bdc0bf30c2f1
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-18 00:50:37 +0300

    Initializing login form class

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,3 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Username()])
+    password = PasswordField('Password', validators=[DataRequired()])
