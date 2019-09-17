e5f0c6603fb4c4d9d30f3d7d41b64c01073522ba
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-18 00:53:50 +0300

    Signup form fields

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -6,3 +6,9 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Username()])
     password = PasswordField('Password', validators=[DataRequired()])
+
+class SignupForm(FlaskForm):
+    username = StringField( 'Username', validators = [DataRequired(), Length(min = 3, max = 18)] )
+    email = StringField( 'Email', validators = [DataRequired(), Email()] )
+    password = PasswordField( 'Password', validators = [DataRequired()] )
+    confirm_password = PasswordField( 'Password', validators = [DataRequired(), EqualTo('password')] )
