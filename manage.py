970601a6b00ecb960bafbb09c37fc253eac3644d
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-17 12:23:25 +0300

    Initializing apps, manager and migrate apps

--- manage.py
+++ manage.py
@@ -1,5 +1,13 @@
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, Server
+from app import create_app, db
+from app.models import User
+
+app=create_app()
+manager =  Manager(app)
+migrate = Migrate(app,db)
+manager.add_command('runserver',Server(use_debugger=True))
+manager.add_command('db',MigrateCommand)
 
 if __name__=="__main__":
     manager.run()
