73c1f190a11579341f154df1cf73ba6f5b0ba5aa
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-17 22:05:55 +0300

    Initializing development and production class

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import os
 
 class Config:
-    DEBUG=True
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:12345@localhost/pitches'
     SECRET_KEY='hjkhkljlekrnfqclknevn;kewrvnk'
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+
+    DEBUG = True
