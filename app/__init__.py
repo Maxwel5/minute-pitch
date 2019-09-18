53dd0b43f4264adc834f63760c999314fe5b514e
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-18 16:41:07 +0300

    Importing packages from classes

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from config import Config
 from flask_login import login_manager,LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
-app =  Flask(__name__)
 bootstrap = Bootstrap()
 db = SQLAlchemy(app)
 mail = Mail()
@@ -12,9 +14,11 @@ login_manager = LoginManager(app)
 login_manager.login_view = "auth.login"
 login_manager.session_protection = "strong"
 def create_app():
+    app =  Flask(__name__)
     app.config.from_object(Config)
     from .main import main as main_blueprint
     from .auth import auth as auth_blueprint
     app.register_blueprint(main_blueprint)
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    
     return app
