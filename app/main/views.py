337567b86fde3135bb684d3cc729d68926590b52
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-18 08:07:37 +0300

    Getting review categories and returning their values

--- app/main/views.py
+++ app/main/views.py
@@ -5,4 +5,10 @@ from . import main
 @main.route('/')
 @login_required
 def index():
-    return render_template('index.html')
+
+    title = 'Home - Just Pitch Out'
+
+    bootcamp_piches = Pitch.get_pitches('bootcamp')
+    trip_piches = Pitch.get_pitches('trip')
+    sports_pitches = Pitch.get_pitches('sports')
+    return render_template('index.html', title=title, bootcamp = bootcamp_pitches, trip = trip_pitches, sports = sports_pitches)
