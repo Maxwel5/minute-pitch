7eecb889343c978bad7e1cedc0d0d63307308819
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-18 08:33:30 +0300

    Function that returns index page and its data

--- app/main/views.py
+++ app/main/views.py
@@ -12,3 +12,18 @@ def index():
     trip_piches = Pitch.get_pitches('trip')
     sports_pitches = Pitch.get_pitches('sports')
     return render_template('index.html', title=title, bootcamp = bootcamp_pitches, trip = trip_pitches, sports = sports_pitches)
+
+@main.route('/bootcamp/pitches')
+def bootcamp():
+    pitches = Pitch.get_pitches(bootcamp)
+    return render_template('bootcamp.html', title = title, pitches = pitches)
+
+@main.route('/trip/pitches')
+def trip():
+    pitches = Pitch.get_pitches(trip)
+    return render_template('trip.html', title = title, pitches = pitches)
+
+@main.route('/sports/pitches')
+def sports():
+    pitches = Pitch.get_pitches(sports)
+    return render_template('sports.html', title = title, pitches = pitches)
