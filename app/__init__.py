d8c69aa6e415981ec1bd18c7b407bfc33a191c42
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-18 16:36:07 +0300

    Declaring arguments

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,10 @@ from flask_login import login_manager,LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app =  Flask(__name__)
+bootstrap = Bootstrap()
 db = SQLAlchemy(app)
+mail = Mail()
+photos = UploadSet('photos',IMAGES)
 login_manager = LoginManager(app)
 login_manager.login_view = "auth.login"
 login_manager.session_protection = "strong"
@@ -13,5 +16,5 @@ def create_app():
     from .main import main as main_blueprint
     from .auth import auth as auth_blueprint
     app.register_blueprint(main_blueprint)
-    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
     return app
