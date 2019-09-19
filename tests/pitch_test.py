04a9a9536f00de2a04e89e0b72557d94094aba23
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-19 10:46:57 +0300

    Subtesting class fuction to define set up test

--- tests/pitch_test.py
+++ tests/pitch_test.py
@@ -1,3 +1,9 @@
 import unittest
 from app.models import Pitch
 Pitch = Pitch
+
+class PitchTest(unittest.TestCase):
+    def setUp(self):
+        sels.new_pitch = Pitch('test all new_pitch')
+
+    
