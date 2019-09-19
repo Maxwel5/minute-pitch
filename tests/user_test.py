7eae39e9301f1998484c765b46c90800f91a7d7e
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-19 11:04:23 +0300

    Functions to define accessing and password verification

--- tests/user_test.py
+++ tests/user_test.py
@@ -1,2 +1,12 @@
 import unittest
-from app.models import user_test
+from app.models import user
+class UserModelTest(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User(password = 'young')
+
+    def test_no_access_password(self):
+            with self.assertRaises(AttributeError):
+                self.new_user.password
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('young'))
