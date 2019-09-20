3638b1118572ace454d72afb08e89b1f51a996b8
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-20 12:50:28 +0300

    Set SMTP server to configure gmail

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 import os
 
 class Config:
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:12345@localhost/pitches'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
