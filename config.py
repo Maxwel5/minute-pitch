d0ff8cfaf9f5a34030e1711c7a8dde4976546e4f
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-20 12:38:29 +0300

    Specifying image storage destination

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:12345@localhost/pitches'
 
 class ProdConfig(Config):
